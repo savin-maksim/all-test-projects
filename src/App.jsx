0eabdfe556735f646a472130ba70fa7942fb7509
savin-maksim <savin.m.a.25pt@gmail.com>
2024-07-06 19:06:18 +0300

    Calculator is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-import React, { useState } from 'react';
+import React from 'react';
 import NavBar from './components/NavBar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SimpleCalc from './pages/SimpleCalc';
+import OldCalc from './pages/OldCalc';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
+const theme = createTheme({
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        html, body, #root, .MuiContainer-root {
+          margin: 0;
+          padding: 0;
+        }
+      `,
+    },
+  },
+});
 
 function App() {
-
   return (
-    <>
-        
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<div>Home</div>}></Route>
+          <Route path="/simple-calc" element={<SimpleCalc />}></Route>
+          <Route path="/old-calc" element={<OldCalc />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
