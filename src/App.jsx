f05c39857f2ca894a47de394a89c3c4622747e36
savin-maksim <savin.m.a.25pt@gmail.com>
2024-07-07 18:31:03 +0300

    Initial movies app

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,37 @@
+import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import './index.scss';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Container } from '@mui/material';
+import { Header } from './components/Header';
+import { FullMovie } from './pages/FullMovie';
 
-const questions = [
-  {
-    title: 'React - это ... ?',
-    variants: ['библиотека', 'фреймворк', 'приложение'],
-    correct: 0,
-  },
-  {
-    title: 'Компонент - это ... ',
-    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
-    correct: 1,
-  },
-  {
-    title: 'Что такое JSX?',
-    variants: [
-      'Это простой HTML',
-      'Это функция',
-      'Это тот же HTML, но с возможностью выполнять JS-код',
-    ],
-    correct: 2,
-  },
-];
-
-function Result() {
-  return (
-    <div className="result">
-      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
-      <button>Попробовать снова</button>
-    </div>
-  );
-}
-
-function Game() {
-  return (
-    <>
-      <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
-      </div>
-      <h1>Что такое useState?</h1>
-      <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
-      </ul>
-    </>
-  );
-}
+export const ColorModeContext = React.createContext();
 
 function App() {
+  const [darkMode, setDarkMode] = React.useState(false);
+
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+    },
+  });
+
   return (
-    <div className="App">
-      <Game />
-      {/* <Result /> */}
+    <div className={`App ${darkMode ? 'App_dark' : ''}`}>
+      <ColorModeContext.Provider value={{ darkMode, setDarkMode }}>
+        <CssBaseline />
+        <ThemeProvider theme={theme}>
+          <Header />
+          <Container maxWidth="lg">
+            <Home />
+            {/* <FullMovie /> */}
+            {/* <About /> */}
+          </Container>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
     </div>
   );
 }
