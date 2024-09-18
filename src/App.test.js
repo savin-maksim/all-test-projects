191583ce4016cb71f47deaf96a5bdec148ca05d9
savin-maksim <savin.m.a.25pt@gmail.com>
2024-09-18 18:10:15 +0300

    initial commit calc v2

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import ComplexCalc from './ComplexCalc';
-
-test('renders learn react link', () => {
-  render(<ComplexCalc />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ComplexCalc from './ComplexCalc';
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ComplexCalc />
+  </React.StrictMode>,
+);
