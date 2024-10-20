fd8534062c7764d78a416326f821fc2901f4a3f1
savin-maksim <savin.m.a.25pt@gmail.com>
2024-10-20 15:27:26 +0300

    initial commit for new keyboard

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,  // Разрешает доступ с других устройств
+  },
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler' // or "modern"
+      }
+    }
+  }
 })
+
